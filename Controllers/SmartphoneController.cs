3adba84075a61b46e00dbfbd0856237d20de8ba3
OREL <slavik2088@gmail.com>
2023-06-06 00:37:19 +0700

    Initial commit for major files

---
+++ Controllers/SmartphoneController.cs
@@ -0,0 +1,49 @@
+﻿using KamilaClient.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KamilaClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmartphoneController : ControllerBase
+    {
+        private static List<Smartphone> _smartphoneList = new List<Smartphone>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Smartphone>> Get()
+        {
+            return _smartphoneList;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Smartphone> Get(int id)
+        {
+            if (_smartphoneList.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            return _smartphoneList[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Smartphone value)
+        {
+            _smartphoneList.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Smartphone value)
+        {
+            if (_smartphoneList.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _smartphoneList[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (_smartphoneList.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _smartphoneList.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Smartphone.cs
@@ -0,0 +1,10 @@
+﻿namespace KamilaClient.Models
+{
+    public class Smartphone
+    {
+        public string Firm { get; set; }
+        public string Model { get; set; }
+        public int Memory { get; set; }
+        public int Price { get; set; }
+    }
+}
